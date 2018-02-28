be723c8b159e779f79e1a67eb7953f0a1159b297
lmazariegos <35360118+lmazariegos@users.noreply.github.com>
2018-02-28 14:41:20 -0600

    Add files via upload

---
+++ assignments/lesson-7/scripts/windchill.js
@@ -0,0 +1,8 @@
+var high = 90;
+var low = 66;
+var tempF = (high + low) / 2;
+var speed = 5;
+function windChill(tempF, speed) {
+	var wChill = (35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16));
+	return wChill;
+}
